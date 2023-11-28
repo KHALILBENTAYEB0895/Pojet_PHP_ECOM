fb6650a6b5cda6cd1b44d0a186b581beb0602279
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2023-11-28 12:18:18 +0100

    afichher categories done

--- front/categorie.php
+++ front/categorie.php
@@ -1,20 +1,24 @@
+<?php
+require_once'../include/pdo.php';
+$id = $_GET['id'];
+$sqlState = $pdo->prepare('SELECT * FROM categorie Where id=?');
+$sqlState->execute([$id]);
+$categorie=$sqlState->fetch(PDO::FETCH_ASSOC);
+var_dump($categorie);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site Ecommerce</title>
+    <title><?php echo $categorie['libelle'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
    <div class="container py-2">
-    <h4>Liste des categories</h4>
-    <?php
-        require_once '../include/pdo.php';
-        $categories=$pdo->query('SELECT * FROM categorie')->fetchAll(PDO::FETCH_OBJ);
-    ?>
-    <div class="list-group">
+    <h4><?php echo $categorie['libelle'] ?></h4>
+    <!-- <div class="list-group">
     <?php
         foreach($categories as $categorie){
             ?>
@@ -24,7 +28,7 @@
             <?php
         }
         ?>
-    </div>
+    </div> -->
    </div>
 </body>
 </html>
