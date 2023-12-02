ba7c8d1fc9d5134515c8e6c61e724d969a73f287
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2023-12-02 14:30:35 +0100

    mise a jour de la fonction Qty

--- assets/js/produit/counter.js
+++ assets/js/produit/counter.js
@@ -1,15 +1,19 @@
-let quantity = 0;
+let quantity = document.getElementById("qty").value;
 
 function updateQuntity(){
     document.getElementById("qty").value = quantity;
 }
 
 function increment(){
-    quantity ++;
-    updateQuntity();
+    quantity = document.getElementById("qty").value;
+    if(quantity < 100){
+        quantity ++;
+        updateQuntity();
+    }
 }
 
 function decrement(){
+    quantity = document.getElementById("qty").value;
     if(quantity > 0){
         quantity --;
         updateQuntity();
