c2be9a4dd0b2fd262b17f063fd182c6e9757b001
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2023-12-02 14:12:00 +0100

    ajout des fichiers JS

--- assets/js/produit/counter.js
+++ assets/js/produit/counter.js
@@ -1,1 +1,17 @@
-alert('hey ok')
+let quantity = 0;
+
+function updateQuntity(){
+    document.getElementById("qty").value = quantity;
+}
+
+function increment(){
+    quantity ++;
+    updateQuntity();
+}
+
+function decrement(){
+    if(quantity > 0){
+        quantity --;
+        updateQuntity();
+    }
+}
